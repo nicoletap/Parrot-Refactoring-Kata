032e5d5da6c0745cb7dac350a189ac7bdf02812c
Your Name <sfauvel@gmail.com>
2023-05-13 23:17:30 +0200

    Configure DocAsTest for Java

---
+++ Java/src/test/java/parrot/ParrotDocTest.java
@@ -0,0 +1,19 @@
+package parrot;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import org.sfvl.doctesting.junitextension.SimpleApprovalsExtension;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ParrotDocTest {
+    @RegisterExtension
+    static SimpleApprovalsExtension doc = new SimpleApprovalsExtension();
+
+
+    @Test
+    public void parrotList() {
+        doc.write("???");
+    }
+
+}
