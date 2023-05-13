63175f0aabcd5c55bc03d85867ef992d8f37297b
Your Name <sfauvel@gmail.com>
2023-05-13 23:40:44 +0200

    Configure DocAsTest for Python

---
+++ Python/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from doc_as_test_pytest import DocAsTest, doc_module
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    """
+    This hook write in doc the exception that occured during test execution.
+    """
+    outcome = yield
+    rep = outcome.get_result()
+
+    if rep.when == "call" and rep.failed:
+        doc_fixture_name = doc_module.__name__
+        if doc_fixture_name in item.funcargs:
+            doc = item.funcargs[doc_fixture_name]
+            if isinstance(doc, DocAsTest):
+                doc.write(f"\n********************\nException executing test:\n----\n{rep.longreprtext}\n----\n")
---
+++ Python/test_doc_parrot.py
@@ -0,0 +1,6 @@
+from parrot import Parrot, ParrotType
+
+from doc_as_test_pytest import DocAsTest, doc, doc_module
+
+def test_parrot_list(doc):
+    doc.write("???")
