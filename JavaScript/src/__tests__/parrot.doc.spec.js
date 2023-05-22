886a2b7b5a6f2ada4ceb173283f78a776537dc4b
Your Name <sfauvel@gmail.com>
2023-05-22 09:28:05 +0200

    Produce adoc files with JavaScript

--- JavaScript/src/__tests__/parrot.doc.spec.js
+++ JavaScript/src/__tests__/parrot.doc.spec.js
@@ -3,11 +3,24 @@ import { Parrot, PARROT_TYPES } from '../parrot';
 const assert = require('assert');
 
 var approvals = require('approvals');
+var ManualNamer = require('approvals/lib/ManualNamer');
+var StringWriter = require("approvals/lib/StringWriter");
+
+var verify = function (dirName, testName, data) {
+  var newOptions = approvals.getConfig();
+  var namer = new ManualNamer(dirName, testName);
+  var writer = new StringWriter(newOptions, data, 'adoc');
+  approvals.verifyWithControl(namer, writer, null, newOptions);
+};
+
+approvals.configure({
+  errorOnStaleApprovedFiles: false
+});
 
 const docs_path="docs"
 
 describe("Parrot", function () {
     it('test with ApprovalTests', function() {
-        approvals.verify(docs_path, 'sample-approval-test', "Document content to change");
+        verify(docs_path, 'sample-approval-test', "Document content to change");
     });
 });
